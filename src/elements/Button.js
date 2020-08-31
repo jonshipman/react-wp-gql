01c9c41cb847a51e20aba2115d8af4a89f297584
Jon Shipman <jon@jonshipman.com>
2020-08-31 15:38:07 -0500

    pass onclick

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const Button = ({ children, to = "/", className = "" }) => {
+export const Button = ({ children, to = "/", onClick, className = "" }) => {
+  if (onClick) {
+    return (
+      <div
+        className={`${className} bg-light-gray pv2 ph3 dib`}
+        onClick={onClick}
+      >
+        {children}
+      </div>
+    );
+  }
   return (
     <div className={`${className} bg-light-gray pv2 ph3 dib`}>
-      <Link to={to}>{children}</Link>
+      <Link to={to} className="color-inherit no-underline">
+        {children}
+      </Link>
     </div>
   );
 };
