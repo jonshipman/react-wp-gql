b9de0556236058839ac7a026c6d38a966190a3c6
Jon Shipman <jon@jonshipman.com>
2020-08-31 16:13:45 -0500

    onChange prop

--- src/elements/FormGroup.js
+++ src/elements/FormGroup.js
@@ -1,12 +1,19 @@
 import React, { forwardRef } from "react";
 
 // Simple placeholder.
-let FormGroup = ({ onEnter = () => {}, ...props }, ref) => {
+let FormGroup = (
+  { onEnter = () => {}, onChange = () => {}, ...props },
+  ref,
+) => {
   props.className = `w-100 db ${props.className || ""}`;
 
   return (
     <div>
-      <input ref={ref} {...props} />
+      <input
+        onChange={(e) => onChange(e.currentTarget.value)}
+        ref={ref}
+        {...props}
+      />
     </div>
   );
 };
