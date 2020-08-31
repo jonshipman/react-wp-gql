da8e2dff47d5507733a6acaa64f5d584e13a3c16
Jon Shipman <jon@jonshipman.com>
2020-08-31 15:33:32 -0500

    added Link to the placeholder button

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export const Button = ({ children, className = "" }) => {
+export const Button = ({ children, to = "/", className = "" }) => {
   return (
-    <div className={`${className} bg-light-gray pv2 ph3 dib`}>{children}</div>
+    <div className={`${className} bg-light-gray pv2 ph3 dib`}>
+      <Link to={to}>{children}</Link>
+    </div>
   );
 };
