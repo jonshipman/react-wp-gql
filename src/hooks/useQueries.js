d5e3d220890050e37f45a112e52ddcfb20d6fbd0
Jon Shipman <jon@jonshipman.com>
2020-09-01 16:40:50 -0500

    Queries are built in the NodeProvider, no need to populate twice

--- src/hooks/useQueries.js
+++ src/hooks/useQueries.js
@@ -1,34 +1,14 @@
 import { useContext } from "react";
 
 import { NodeContext } from "../Context";
-import * as DefaultQueries from "../gql/queries";
-import * as DefaultFragments from "../gql/fragments";
 
 const useFragments = () => {
-  const { fragments } = useContext(NodeContext);
-
-  if (!fragments) fragments = {};
-
-  Object.keys(DefaultFragments).forEach((key) => {
-    if (!fragments[key]) {
-      fragments[key] = DefaultFragments[key];
-    }
-  });
-
-  return { fragments };
+  return useContext(NodeContext);
 };
 
 export const useQueries = () => {
   const { queries } = useContext(NodeContext);
   const { fragments } = useFragments();
-
-  if (!queries) queries = {};
-
-  Object.keys(DefaultQueries).forEach((key) => {
-    if (!queries[key]) {
-      queries[key] = DefaultQueries[key];
-    }
-  });
 
   const executedQueries = {};
 
