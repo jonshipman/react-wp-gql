e5b0da4d15346b277c0eb5f01f8cba71d537854b
Jon Shipman <jon@jonshipman.com>
2020-09-01 09:27:11 -0500

    provide a default pagewidth inside Title

--- src/elements/Title.js
+++ src/elements/Title.js
@@ -1,20 +1,22 @@
 import React, { forwardRef, createElement } from "react";
 
 import { useComponents } from "../hooks/useComponents";
+import { PageWidth } from "./PageWidth";
 
 let Title = ({ notHeading, className = "", children, ...props }, ref) => {
   const { components } = useComponents();
+  const PageWidthLocal = components.PageWidth || PageWidth;
 
   const Wrap = createElement(notHeading ? "div" : "h1", {});
   return (
     <div className={`bg-near-white ${className}`} {...props}>
-      <components.PageWidth>
+      <PageWidthLocal>
         <Wrap.type className="title ma0 lh-solid pv4">
           <span className="f4 fw4 db" ref={ref}>
             {children}
           </span>
         </Wrap.type>
-      </components.PageWidth>
+      </PageWidthLocal>
     </div>
   );
 };
