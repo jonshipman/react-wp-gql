f1cb09a16255845ccb159c5c14c8f44a66cf9ad3
Jon Shipman <jon@jonshipman.com>
2020-09-16 11:31:44 -0500

    post meta render issue

--- src/elements/SingleRender.js
+++ src/elements/SingleRender.js
@@ -16,6 +16,7 @@ export const SingleRender = ({ node = {}, postPreload }) => {
   } = node;
 
   const { components } = useComponents();
+  const loadPostMeta = __typename === "Post" || postPreload;
 
   return (
     <article className={`single post-${databaseId}`}>
@@ -26,63 +27,62 @@ export const SingleRender = ({ node = {}, postPreload }) => {
         breadcrumbs={seo.breadcrumbs}
       />
       <components.PageWidth className="mv4">
-        {__typename === "Post" ||
-          (postPreload && (
-            <React.Fragment>
-              <h1 className="f2 fw4 mb4">
-                {title ? title : <components.SkullLine />}
-              </h1>
+        {loadPostMeta && (
+          <React.Fragment>
+            <h1 className="f2 fw4 mb4">
+              {title ? title : <components.SkullLine />}
+            </h1>
 
-              <div className="post-meta mv4">
-                <div className="posted dib mr4">
-                  <components.ClockIcon
-                    className="mr2 v-mid"
-                    width={20}
-                    height={20}
-                  />
-                  {dateFormatted ? (
-                    <span>{dateFormatted}</span>
-                  ) : (
-                    <components.SkullWord />
-                  )}
-                </div>
-
-                <div className="post-categories dib">
-                  <components.FolderIcon
-                    className="mr2 v-mid"
-                    width={20}
-                    height={20}
-                  />
-                  <ul className="list pl0 dib">
-                    {categories?.edges?.length > 0 ? (
-                      categories.edges.map((category) => (
-                        <li
-                          key={`cat-${category.node.databaseId}-post-cats`}
-                          className="dib mr2 pr2 br b--near-white drop-last-br"
-                        >
-                          <Link
-                            to={category.node.uri}
-                            className="primary no-underline"
-                          >
-                            {category.node.name}
-                          </Link>
-                        </li>
-                      ))
-                    ) : (
-                      <React.Fragment>
-                        <li className="dib mr2 pr2 br b--near-white drop-last-br">
-                          <components.SkullWord />
-                        </li>
-                        <li className="dib mr2 pr2 br b--near-white drop-last-br">
-                          <components.SkullWord />
-                        </li>
-                      </React.Fragment>
-                    )}
-                  </ul>
-                </div>
+            <div className="post-meta mv4">
+              <div className="posted dib mr4">
+                <components.ClockIcon
+                  className="mr2 v-mid"
+                  width={20}
+                  height={20}
+                />
+                {dateFormatted ? (
+                  <span>{dateFormatted}</span>
+                ) : (
+                  <components.SkullWord />
+                )}
               </div>
-            </React.Fragment>
-          ))}
+
+              <div className="post-categories dib">
+                <components.FolderIcon
+                  className="mr2 v-mid"
+                  width={20}
+                  height={20}
+                />
+                <ul className="list pl0 dib">
+                  {categories?.edges?.length > 0 ? (
+                    categories.edges.map((category) => (
+                      <li
+                        key={`cat-${category.node.databaseId}-post-cats`}
+                        className="dib mr2 pr2 br b--near-white drop-last-br"
+                      >
+                        <Link
+                          to={category.node.uri}
+                          className="primary no-underline"
+                        >
+                          {category.node.name}
+                        </Link>
+                      </li>
+                    ))
+                  ) : (
+                    <React.Fragment>
+                      <li className="dib mr2 pr2 br b--near-white drop-last-br">
+                        <components.SkullWord />
+                      </li>
+                      <li className="dib mr2 pr2 br b--near-white drop-last-br">
+                        <components.SkullWord />
+                      </li>
+                    </React.Fragment>
+                  )}
+                </ul>
+              </div>
+            </div>
+          </React.Fragment>
+        )}
 
         {content ? (
           <components.PostContent>{content}</components.PostContent>
