8504eedf07befed2b882fac7c27af48491cfd118
Jon Shipman <jon@jonshipman.com>
2020-11-04 11:27:13 -0600

    return the component on ssr instead of fallback

--- src/elements/Suspense.js
+++ src/elements/Suspense.js
@@ -11,7 +11,7 @@ export const Suspense = ({ fallback = <Loading />, ...props }) => {
   }, []);
 
   if (!isFront) {
-    return null;
+    return props?.children || null;
   }
 
   return <Source {...{ fallback }} {...props} />;
