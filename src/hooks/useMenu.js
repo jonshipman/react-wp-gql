e43146845ce63d0be717c0a14e5f7b2785ed0a93
Jon Shipman <jon@jonshipman.com>
2021-01-11 16:25:43 -0600

    save the menu in local storage

--- src/hooks/useMenu.js
+++ src/hooks/useMenu.js
@@ -1,12 +1,18 @@
 import { useQuery } from "@apollo/client";
-
 import { useQueries } from "./useQueries";
 
 export const useMenu = (props = {}) => {
   const { queries } = useQueries();
-
   const { location = "HEADER_MENU", parentId = 0, ...queryProps } = props;
   const variables = { location, parentId };
+
+  const MenuLocalStorage = useMemo(
+    () =>
+      window.localStorage
+        ? window.localStorage.getItem(`useMenu_${location}`)
+        : [],
+    [location],
+  );
 
   const { data, loading, error } = useQuery(queries.QueryMenu, {
     variables,
@@ -14,7 +20,24 @@ export const useMenu = (props = {}) => {
     ...queryProps,
   });
 
-  const menuItems = data ? data.menuItems?.nodes || [] : [];
+  const menuItems = useMemo(() => {
+    if (data?.menuItems?.nodes) {
+      if (window.localStorage) {
+        window.localStorage.setItem(
+          `useMenu_${location}`,
+          JSON.stringify(data?.menuItems?.nodes),
+        );
+      }
+
+      return data?.menuItems?.nodes || [];
+    }
+
+    if (MenuLocalStorage) {
+      return JSON.parse(MenuLocalStorage);
+    }
+
+    return [];
+  }, [data, MenuLocalStorage, location]);
 
   return {
     menuItems,
