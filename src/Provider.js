afee35f138869869dfd11aa39217dc9a5c9f85d8
Jon Shipman <jon@jonshipman.com>
2021-01-14 15:46:21 -0600

    removed apollo setup from library, that should be done in parent app

--- src/Provider.js
+++ src/Provider.js
@@ -1,9 +1,6 @@
 import React from "react";
-import { ApolloProvider } from "@apollo/client";
-
 import { Populate } from "./Defaults";
 import { NodeContext } from "./Context";
-import { ApolloSetup } from "./ApolloSetup";
 
 export const NodeProvider = ({
   children,
@@ -11,28 +8,18 @@ export const NodeProvider = ({
   fragments = {},
   queries = {},
   mutations = {},
-  client: clientProp,
-  gqlUrl,
-  cache,
-  links,
   ...props
 }) => {
   const refactored = Populate({ components, fragments, queries, mutations });
 
-  const client = clientProp
-    ? clientProp
-    : ApolloSetup({ gqlUrl, cache, links });
-
   return (
-    <ApolloProvider {...{ client }}>
-      <NodeContext.Provider
-        value={{
-          ...refactored,
-          ...props,
-        }}
-      >
-        {children}
-      </NodeContext.Provider>
-    </ApolloProvider>
+    <NodeContext.Provider
+      value={{
+        ...refactored,
+        ...props,
+      }}
+    >
+      {children}
+    </NodeContext.Provider>
   );
 };
