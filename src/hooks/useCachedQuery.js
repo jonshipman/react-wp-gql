1f2fd530abff644e21b223cd53cff381e5c5dc4d
Jon Shipman <jon@jonshipman.com>
2021-01-14 16:15:28 -0600

    went ahead and added memo

--- src/hooks/useCachedQuery.js
+++ src/hooks/useCachedQuery.js
@@ -1,13 +1,18 @@
 import { useQuery } from "@apollo/client";
+import { useMemo } from "react";
 
 export const useCachedQuery = (key, ...props) => {
   const { data: queryData, ...results } = useQuery(...props);
 
-  const data = !!queryData
-    ? queryData
-    : !!window.localStorage.getItem(key)
-    ? JSON.parse(window.localStorage.getItem(key))
-    : queryData;
+  const data = useMemo(
+    () =>
+      !!queryData
+        ? queryData
+        : !!window.localStorage.getItem(key)
+        ? JSON.parse(window.localStorage.getItem(key))
+        : queryData,
+    [queryData],
+  );
 
   useEffect(() => {
     if (!!queryData) {
